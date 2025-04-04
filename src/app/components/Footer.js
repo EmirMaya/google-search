04397a3756d8a296838809651825c3aad8c17e7c
Emir Maya <emirmayaok@gmail.com>
2025-04-04 20:44:54 -0300

    detealing footer

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -3,14 +3,24 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="mt-44 grid w-full divide-y-[1px] divide-gray-300 bg-gray-100">
+    <footer className="mt-44 grid w-full divide-y-[1px] divide-gray-300 bg-gray-100 text-sm text-gray-500">
       <div className="px-8 py-3">
         <h3>Acerca de Google</h3>
       </div>
-      <div>
+      <div className="grid grid-cols-1">
         <div>
             <GlobeAmericasIcon className="h-5"/>
           <p>Carbon neutral since 2007</p>
+        </div>
+        <div className="flex justify-center space-x-8 whitespace-nowrap">
+            <p>Advertising</p>
+            <p>Business</p>
+            <p>How Search Works</p>
+        </div>
+        <div className="flex justify-center space-x-8 whitespace-nowrap">
+            <p>Privacy</p>
+            <p>Terms</p>
+            <p>Settings</p>
         </div>
       </div>
     </footer>
